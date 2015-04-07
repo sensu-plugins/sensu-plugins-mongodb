eefd4ba915fd3f3a1bc3249503fa60f1d86cce03
Matt Jones <mattyjones@users.noreply.github.com>
2015-04-07 08:46:31 -0400

    bump

--- lib/sensu-plugins-mongodb/version.rb
+++ lib/sensu-plugins-mongodb/version.rb
@@ -8,7 +8,7 @@ module SensuPluginsMongoDB
     MINOR = 0
     PATCH = 1
 
-    VER_STRING = [MAJOR, MINOR, PATCH, 'alpha.1'].compact.join('.')
+    VER_STRING = [MAJOR, MINOR, PATCH, 'alpha.2'].compact.join('.')
 
     NAME   = 'sensu-plugins-mongodb'
     BANNER = "#{NAME} v%s"
