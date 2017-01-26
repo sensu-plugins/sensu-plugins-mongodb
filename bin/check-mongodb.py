d4485444ae2f75ee6ee4d0852066225e68cbdd9e
Nicolas Boutet <amd3002@gmail.com>
2017-01-26 10:12:25 +0800

    Fix 'General MongoDB Error: can't set attribute'

--- bin/check-mongodb.py
+++ bin/check-mongodb.py
@@ -319,7 +319,9 @@ def exit_with_general_critical(e):
 
 
 def set_read_preference(db):
-    if pymongo.version >= "2.1":
+    if pymongo.version >= "2.2":
+        pymongo.read_preferences.Secondary
+    else:
         db.read_preference = pymongo.ReadPreference.SECONDARY
 
 
